f1b90e452e5e15478a4dada03ebe370054da3920
Arundhati Vibhute <arundhatik0@gmail.com>
2025-04-14 08:04:01 -0500

    Anonymous Functions and Functional Programming - 1

--- project-task.js
+++ project-task.js
@@ -29,7 +29,7 @@ const todos = [
   // ============================================
   // 🔍 Tasks
   // ============================================
-  
+
   /*
   🔹 Task 1: Filter Incomplete Tasks
   
@@ -38,8 +38,11 @@ const todos = [
   2. Use an anonymous function as the callback.
   3. Return only the tasks that are not completed.
   */
-  
-  
+function incompleteTasks() {
+    const incompleteTodos = todos.filter(todo => !todo.completed);
+    return incompleteTodos
+}
+ 
   /*
   🔹 Task 2: Sort Tasks by Priority
   
@@ -48,7 +51,11 @@ const todos = [
   2. Use an anonymous function as the comparison function.
   3. Sort tasks in ascending order of priority (1 = highest).
   */
-  
+function sortTask() {
+    
+    const sortedTask = [...todos].sort((a, b) => a.priority - b.priority);     // created copy of array and sort it ..not changed in original array   
+    return sortedTask;
+}
   
   /*
   🔹 Task 3: Mark All Tasks as Completed
@@ -58,6 +65,14 @@ const todos = [
   2. Use an anonymous function to modify each object.
   3. Change the `completed` property to `true` for every task.
   */
+function completedTask() {
+
+    const updatedTodos = todos.map(todo => ({             //{ ...todo, completed: true } makes a copy of each task and updates completed.
+        ...todo,
+        completed: true
+    }));
+    return updatedTodos;
+}
   
   
   /*
@@ -68,14 +83,37 @@ const todos = [
   2. Then, sort the filtered results by priority using `sort()`.
   3. Use method chaining to perform both steps together.
   */
-  
+
+function sortedIncomplete() {
+    
+    const sortedIncomplete = todos
+        .filter(todo => !todo.completed)
+        .sort((a, b) => a.priority - b.priority);
+    return sortedIncomplete;
+}
   
   // ============================================
   // 🧪 Console Test Your Work
   // ============================================
   
-  // console.log("Incomplete Tasks:", ...);
-  // console.log("Sorted by Priority:", ...);
-  // console.log("All Tasks Completed:", ...);
-  // console.log("Sorted Incomplete Tasks:", ...);
+console.log("Incomplete Tasks:");
+
+for (todo of incompleteTasks()) {
+    console.log(`${todo.task}`);
+}
+console.log("Sorted by Priority:");
+
+for (todo of sortTask()) {
+    console.log(`${todo.task} | ${todo.priority}`);
+}
+console.log("All Tasks Completed:");
+
+for (todo of completedTask()) {
+    console.log(`${todo.task} | ${todo.completed}`);
+}
+
+console.log("Sorted Incomplete Tasks:");
+for (todo of sortedIncomplete()) {
+    console.log(`${todo.task} | ${todo.completed}`);
+}
   
